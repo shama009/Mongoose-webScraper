362dc2a746e114119256f4b504aa7c2a6ef2127c
Shama <shamaanjum09@outlook.com>
2018-02-01 15:22:11 -0800

    update and delete routes

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ var app = express();
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 // Database configuration
@@ -34,94 +35,98 @@ mongoose.connect(MONGODB_URI);
 
 // A GET route for scraping the nytimes website
 app.get("/scrape", function (req, res) {
-    // First, we grab the body of the html with request
-    axios.get("http://www.nytimes.com/").then(function (response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
-  
-      // Now, we grab every h2 within an article tag, and do the following:
-      $("article h2").each(function (i, element) {
-        // Save an empty result object
-        var result = {};
-  
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
-  
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function (dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function (err) {
-            // If an error occurred, send it to the client
-            return res.json(err);
-          });
-      });
-  
-      // If we were able to successfully scrape and save an Article, send a message to the client
-      res.send("Scrape Complete");
+  // First, we grab the body of the html with request
+  axios.get("http://www.nytimes.com/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function (i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
     });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
   });
+});
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-    // Grab every document in the first 20 Articles collection
-    db.Article.find({}).limit(20)
-      .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
-  //update saved article in db
-  app.post("/api/article/update", function(req, res){
-    
-    // need to use JSON.parse here to get the string of "false" or "true" and make it boolean false or true
-    var isSaved = (JSON.parse(req.body.isSaved)) 
-    
-    db.Article.findByIdAndUpdate(req.body.id, {
-        $set: {
-            "isSaved": isSaved
-        }
-    }).then(function(data){
-        res.redirect("/");
-    }).catch(function(err){
-        res.json(err);
+app.get("/articles", function (req, res) {
+  // Grab every document in the first 20 Articles collection
+  db.Article.find({}).limit(20)
+    .then(function (dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+//update saved article in db
+app.post("/api/article/update", function (req, res) {
+
+  // need to use JSON.parse here to get the string of "false" or "true" and make it boolean false or true
+  // var isSaved = (JSON.parse(req.body.isSaved));
+  var isSaved = req.body.isSaved;
+  db.Article.findByIdAndUpdate(req.body.id, {
+    $set: {
+      "isSaved": isSaved
+    }
+  }, { new: true })
+    .then(function (data) {
+      //res.redirect("/");
+      console.log("updated");
+      console.log(data);
+      res.json(data);
+    }).catch(function (err) {
+      res.json(err);
     });
 });
 
 // Route for getting all Saved Articles from the db
-app.get("/savedArticles", function(req, res) {
-    // Grab every document in the first 20 Articles collection
-    db.Article.find({isSaved: true})
-      .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
-  // delete articles by id
-  app.post("/api/articles/delete", function(req, res){
-    db.Student.findByIdAndRemove(req.body.id).then(function(data){
-        res.redirect("/");
-    }).catch(function(err){
-        res.json(err);
+app.get("/savedArticles", function (req, res) {
+  // Grab every document in the first 20 Articles collection
+  db.Article.find({ isSaved: true })
+    .then(function (dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
     });
+});
+// delete articles by id
+app.post("/api/articles/delete", function (req, res) {
+  db.Student.findByIdAndRemove(req.body.id).then(function (data) {
+    res.redirect("/");
+  }).catch(function (err) {
+    res.json(err);
+  });
 });
 
 // Start the server
 app.listen(PORT, function () {
-    console.log("App running on port " + PORT + "!");
+  console.log("App running on port " + PORT + "!");
 });
